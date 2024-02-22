dcc99010dab973affc0ebc18f7ea5556734b4bf7
Doctor-Where <159565673+Doctor-Where@users.noreply.github.com>
2024-02-22 08:46:02 -0800

    changed theme, updated counter

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 var count = 1;
+var power = 0;
 
 function onCounterClick() {
     count *= 2;
-    document.getElementById("counter").textContent = "Counter: " + count;
+    power += 1;
+    document.getElementById("counter").textContent = "2 to the power of " + power + ": " + count;
 }
